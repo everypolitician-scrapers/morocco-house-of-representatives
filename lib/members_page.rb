cb6f432a71408be33b55fc6dfacfd69e63cfed33
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-29 17:54:01 +0000

    Initialize MemberSection with existing response
    MembersPage already has a response object to use and MemberSection is part of the same page so no need to create a new one.

--- lib/members_page.rb
+++ lib/members_page.rb
@@ -3,7 +3,7 @@ require 'scraped'
 class MembersPage < Scraped::HTML
   field :members do
     noko.css('ul.lisitng_resultat li').map do |li|
-      MemberSection.new(response: Scraped::Request.new(url: url).response, noko: li)
+      MemberSection.new(response: response, noko: li)
     end
   end
 end
