8f32daaa94f1c5e800741e87e6332f9cee85bd98
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-29 17:56:30 +0000

    Remove unnecessary print statement
    No useful information to be gained from it. Makes morph unhappy.

--- scraper.rb
+++ scraper.rb
@@ -29,7 +29,6 @@ def scrape_list(url)
   MembersPage.new(response: Scraped::Request.new(url: url).response, noko: noko)
              .members
              .each do |member|
-                puts member
                 ScraperWiki.save_sqlite([:id, :term], member.to_h) unless member.name == 'Vaccant Poste'
               end
 
