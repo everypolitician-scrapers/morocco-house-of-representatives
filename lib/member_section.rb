13b27140b3d4c9697da286b3d3efdaf5d1df005f
Oliver Denman <oliver@therewillbeanimals.com>
2016-11-29 16:42:05 +0000

    Extract member section to class
    We are capturing data from the members list pages. Member data
    is captured from sub sections of these pages. MemberSection represents
    an individual section.

---
+++ lib/member_section.rb
@@ -0,0 +1,32 @@
+require 'scraped'
+
+class MemberSection < Scraped::HTML
+  field :id do
+    noko.css('h2.name a/@href').text.split("/").last
+  end
+
+  field :name do
+    return 'Kamal Abdel Fattah' if id == 'akamal' # No name in English version
+    noko.css('h2.name').text.tidy
+  end
+
+  field :party do
+    noko.css('a.link span').first.text
+  end
+
+  field :faction do
+    noko.css('a.link span').last.text
+  end
+
+  field :source do
+    noko.css('h2.name a/@href').text
+  end
+
+  field :image do
+    noko.css('a.figure img/@src').text
+  end
+
+  field :term do
+    9
+  end
+end
